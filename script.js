23672958de6b1f2e97d013a4b7b95212a8e6ba7a
Daniel DiCocco <dicoccod@bc.edu>
2020-10-28 15:49:36 -0400

    load data

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let coffeeData;
-let type = document.querySelector("#group-by").value;
-let sort = true;
+
+// Loading data
+d3.csv('unemployment.csv', d3.autoType).then(data=>{ 
+
+    console.log('data', data);    
+
+    //calculate total unemployment
+    let r, item;
+    for (r = 0; r < data.length; r++){
+        let Total = 0;
+        for (item in data[r]){
+            if (item != "date"){
+            Total = Total + data[r][item];
+            }
+         }
+         data[r].total = Total;
+     }
+ 
+     console.log(data); //verify it's been added
+ 
+ 
+
+  
+
+})
+
+
 
 // CHART INIT ------------------------------
 //construct margin
@@ -117,31 +141,3 @@ function update(coffeeData, type, sort){
 }
 
 // CHART UPDATES ---------------------------
-// Loading data
-d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{ 
-
-    data.sort((a, b) => b.stores - a.stores);     //sort data desc. by # stores
-    
-    console.log('data', data); // simply call the update function with the supplied data
-    
-    update(data, type, sort);
-
-    d3.select(".sort-button").on("click", () =>{
-        sort = !sort;
-        update(data, type, sort);
-    });
-
-    d3.select("#group-by").on("change", (e) => {
-            type = e.target.value;
-            update(data, type, sort);
-    });
-
-  
-
-})
-
-// (Later) Handling the type change
-
-// (Later) Handling the sorting direction change
-
-
